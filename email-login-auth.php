30a06b62cc48405a20cd35c0e0affe31bc327f8f
mikk_cz <mikk_cz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-31 11:49:28 +0000

    Version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/email-login-auth/trunk@1171289 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-login-auth.php
+++ email-login-auth.php
@@ -3,7 +3,7 @@
 Plugin Name:	Email Login Auth
 Plugin URI:		https://github.com/MikkCZ/email-login-auth/
 Description:	Enables to login with WordPress user e-mail address.
-Version:		0.1
+Version:		0.2
 Author:			Michal Stanke
 Author URI:		http://www.mikk.cz/
 License:		GPL2
@@ -15,7 +15,7 @@ define( 'EMAIL_LOGIN_AUTH_PLUGIN_FILE', __FILE__ );
 
 require_once 'email-login-auth-options.php';
 
-if ( get_option( 'email-login-auth-email' ) ) {
+if ( get_option( 'email-login-auth-email', email_login_auth_get_default('email-login-auth-email') ) ) {
 	function email_login_auth( $user, $username, $password ) {
 		if ( is_email( $username ) ) {
 			$user_by_email = get_user_by( 'email', $username );
@@ -27,6 +27,10 @@ if ( get_option( 'email-login-auth-email' ) ) {
 		return wp_authenticate_username_password( $user, $username, $password );
 	}
 	add_filter( 'authenticate', 'email_login_auth', 20, 3 );
+
+	if ( ! get_option( 'email-login-auth-username', email_login_auth_get_default('email-login-auth-username') ) ) {
+		remove_filter( 'authenticate', 'wp_authenticate_username_password', 20, 3 );
+	}
 
 	function email_login_auth_username_label( $translated_text, $untranslated_text, $domain ) {
 		if ( $untranslated_text == 'Username' ) {
