7ef0e80cdca22889fda7298a6abbc864701c18dd
mikk_cz <mikk_cz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-31 11:49:28 +0000

    Version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/email-login-auth/tags/0.2@1171289 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ email-login-auth-options.php
@@ -0,0 +1,85 @@
+<?php
+
+defined( 'ABSPATH' ) or die();
+
+function email_login_auth_get_default( $option ) {
+	$defaults = array(
+		'email-login-auth-email' => true,
+		'email-login-auth-username' => true,
+	);
+	if ( isset( $defaults[ $option ] ) ) {
+		return $defaults[ $option ];
+	} else {
+		return false;
+	}
+}
+
+register_activation_hook( EMAIL_LOGIN_AUTH_PLUGIN_FILE, 'email_login_auth_plugin_install' );
+if ( is_admin() ) {
+	add_action( 'admin_init', 'register_email_login_auth_plugin_settings' );
+	add_action( 'admin_menu', 'add_email_login_auth_plugin_menu' );
+}
+
+function email_login_auth_plugin_install() {
+	add_option( 'email-login-auth-email', email_login_auth_get_default('email-login-auth-email') );
+	add_option( 'email-login-auth-username', email_login_auth_get_default('email-login-auth-username') );
+}
+
+function register_email_login_auth_plugin_settings() {
+	register_setting( 'email-login-auth-option-group', 'email-login-auth-email' );
+	register_setting( 'email-login-auth-option-group', 'email-login-auth-username' );
+}
+
+function add_email_login_auth_plugin_menu() {
+	add_options_page(
+		get_email_login_auth_plugin_name(),
+		get_email_login_auth_plugin_name(),
+		'manage_options',
+		'email_login_auth',
+		'email_login_auth_settings_page'
+	);
+}
+
+function email_login_auth_settings_page() {
+	if ( ! get_option( 'email-login-auth-email', email_login_auth_get_default('email-login-auth-email') ) ) {
+		update_option( 'email-login-auth-username', email_login_auth_get_default('email-login-auth-username') );
+	}
+	print( '<div class="wrap">' );
+	printf( '<h2>%s</h2>', get_email_login_auth_plugin_name() );
+	print( '<form method="post" action="options.php">' );
+	settings_fields( 'email-login-auth-option-group' );
+	do_settings_sections( 'email-login-auth-option-group' );
+	print( '<table class="form-table">' );
+	printf( '<tr>
+				<th><label for="email-login-auth-email">Enable login with e-mail</label></th>
+				<td>
+					<input type="checkbox" name="email-login-auth-email" id="email-login-auth-email"%s>
+				</td>
+			</tr>
+			',
+			email_login_auth_checked( 'email-login-auth-email' )
+		);
+	printf( '<tr>
+				<th><label for="email-login-auth-username">Enable login with username</label></th>
+				<td>
+					<input type="checkbox" name="email-login-auth-username" id="email-login-auth-username"%s>
+					<p class="description">If \'Enable login with e-mail\' is off, this will be enabled automatically.</p>
+				</td>
+			</tr>
+			',
+			email_login_auth_checked( 'email-login-auth-username' )
+		);
+	print( '</table>' );
+	submit_button();
+	print( '</form>' );
+	print( '</div>' );
+}
+
+function email_login_auth_checked( $option ) {
+	if ( get_option( $option, email_login_auth_get_default($option) ) ) {
+		return ' checked';
+	} else {
+		return '';
+	}
+}
+
